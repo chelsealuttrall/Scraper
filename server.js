04a201a457c875716d50216a6ceedda666334e5f
chelsealuttrall <chelsealuttrall@gmail.com>
2020-05-03 13:27:24 -0600

    got it working...need to make my own

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var expresshb = require("express-handlebars")
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
@@ -12,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT =process.env.PORT || 3000;
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
@@ -27,42 +26,36 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true,
+});
 
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.bicycling.com/health-nutrition/#").then(function(response) {
+  axios.get("http://www.echojs.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("div.simple_item liste-header-item list-header-small-item").each(function(i, element) {
+    $("article h2").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, log it
           console.log(err);
         });
@@ -74,58 +67,47 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+app.get("/articles", function (req, res) {
   db.Article.find({})
-  .then(function(dbArticle)
-  {
-    res.json(dbArticle);
-  }) 
-  .catch(function(err)
-  {
-    res.json(err);
-  });
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      resp.json(err);
+    });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  db.Article.findOne({_id: req.params.id})
-
-  .populate("note")
-  .then(function(dbArticle) {
-    res.json(dbArticle);
-  })
-.catch(function(err){
-  res.json(err);
-});
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-
-  // save the new note that gets posted to the Notes collection
+app.post("/articles/:id", function (req, res) {
   db.Note.create(req.body)
-  // then find an article from the req.params.id
-  .then(function(dbNote){
-    return db.Article.findOneAndUpdate({_id: req.params.id}, {$set:{note: dbNote._id}}, {new:true})
-  })
-  // and update it's "note" property with the _id of the new note
-  .then(function(dbArticle){
-    res.json(dbArticle);
-  })
-  .catch(function(err) {
-    res.json(err);
-  })
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: { note: dbNote._id } },
+        { new: true }
+      );
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
